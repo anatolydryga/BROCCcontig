b4f5d29ccab3c03a465c1a40b683bd64404e285e
Anatoly Dryga <anatolydryga@gmail.com>
2016-01-25 12:43:25 -0500

    running blast

--- run_blast.sh
+++ run_blast.sh
@@ -4,8 +4,8 @@ NT_BLAST_DB=/media/THING1/local/genomeIndexes/blast_nt/nt
 
 while read line
 do
-    echo $line
     OUTPUT_FILE=blasted_$(basename $line)
     OUTPUT_FILE=${OUTPUT_FILE%.*}.out
+    echo $OUTPUT_FILE
     blastn -query $line -evalue 1e-5 -outfmt 7 -db $NT_BLAST_DB -out $OUTPUT_FILE -num_threads 8 -max_target_seqs 100
-done < sequences.inp
+done < $1
