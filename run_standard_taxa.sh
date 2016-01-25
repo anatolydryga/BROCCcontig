b980f2c39fc36cdac33efdfc4391d02fb835b4a9
Anatoly Dryga <anatolydryga@gmail.com>
2016-01-25 13:00:47 -0500

    std

---
+++ run_standard_taxa.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+while read line
+do
+    OUTPUT_FILE=std_taxa_$(basename $line)
+    OUTPUT_FILE=${OUTPUT_FILE%.*}.txt
+    echo $OUTPUT_FILE
+    Rscipt split_standard_taxa.R ${line}/Standard_Taxonomy.txt $OUTPUT_FILE
+done < $1
