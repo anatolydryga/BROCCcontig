26cfbf022f084ebf2dee5de63596352eca8ef06b
Anatoly Dryga <anatolydryga@gmail.com>
2016-03-01 14:05:35 -0500

    bug fix: ranks include Domain

--- split_standard_taxa.R
+++ split_standard_taxa.R
@@ -7,7 +7,8 @@ input_file <- commandArgs(trailingOnly=TRUE)[1]
 output_file <- commandArgs(trailingOnly=TRUE)[2]
 
 input_taxa <- read.delim(input_file, header=FALSE, stringsAsFactors = FALSE)
-standard_taxa <- split_assignments(input_taxa$V2, split=";")
+standard_taxa <- split_assignments(input_taxa$V2, split=";", 
+    ranks=c("Domain", taxonomic_ranks))
 seq_taxa <- data.frame(seqName=input_taxa$V1, standard_taxa)
 write.table(seq_taxa, file=output_file, quote=FALSE, sep='\t', col.names = NA)
 
