b04551d187bb5b97d7185a24c051cb04b210ae14
Anatoly Dryga <anatolydryga@gmail.com>
2016-04-29 11:38:55 -0400

    params adjustment

--- run_brocc.py
+++ run_brocc.py
@@ -9,4 +9,4 @@ for line in open(sys.argv[1]):
     output_dir = "BROCC_" + seq_basename
     print output_dir
     #brocc.py -i <SEQUENCES (FASTA FORMAT)> -b <BLAST RESULTS> -o <OUTPUT DIRECTORY>
-    call(["brocc.py", "-i", sequence_file, "-b", blast_file, "-o", output_dir, "--min_species_id=60", "--min_genus_id=40" ]) 
+    call(["brocc.py", "-i", sequence_file, "-b", blast_file, "-o", output_dir, "--min_cover=0.2", "--min_id=20", "--min_species_id=60", "--min_genus_id=40" ]) 
